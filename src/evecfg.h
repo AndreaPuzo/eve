fe3290bf3a92e1ed953af59338349482434fa401
AndreaPuzo <andreapz3010@gmail.com>
2023-12-09 17:20:42 +0100

    removing env and lib, setting API constants

--- src/evecfg.h
+++ src/evecfg.h
@@ -1,4 +1,23 @@
 #ifndef __EVECFG_H
 # define __EVECFG_H
 
+# ifdef _WIN32
+#  ifdef __GNUC__
+#   define __EVE_API_EXPORT __attribute__((dllexport))
+#   define __EVE_API_IMPORT __attribute__((dllimport))
+#  else
+#   define __EVE_API_EXPORT __declspec(dllexport)
+#   define __EVE_API_IMPORT __declspec(dllimport)
+#  endif
+# else
+#  define __EVE_API_EXPORT __attribute__((visibility("default")))
+#  define __EVE_API_IMPORT __attribute__((visibility("default")))
+# endif
+
+# ifdef __EVE_BUILD
+#  define __EVE_API __EVE_API_EXPORT
+# else
+#  define __EVE_API __EVE_API_IMPORT
+# endif
+
 #endif
